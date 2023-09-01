c7438ede4d8a87f71f6f583b15f5db284df42b53
Haridokku <124425442+Haridokku@users.noreply.github.com>
2023-09-01 23:10:52 +0530

    Update index.js

--- src/components/MoviesSlider/index.js
+++ src/components/MoviesSlider/index.js
@@ -1,20 +1,29 @@
 import Slider from 'react-slick'
 
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 import MovieItem from '../MovieItem'
 
 const MoviesSlider = props => {
   const {detailsList} = props
-  console.log(detailsList)
+
+  const settings = {
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  }
 
   return (
     <>
-        <Slider>
-          {detailsList.map(each => (
-            <MovieItem key={each.id} movieDetail={each} />
-          ))}
-        </Slider>
+      <Slider {...settings}>
+        {detailsList.map(each => (
+          <MovieItem key={each.id} movieDetail={each} />
+        ))}
+      </Slider>
     </>
   )
 }
 
 export default MoviesSlider
+
